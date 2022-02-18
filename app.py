af7d5298b313043f4652c60bbf1aecd46468a97b
BigSethi <75171192+BigSethi@users.noreply.github.com>
2022-02-18 16:30:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def player_moved(movementData, methods=['GET', 'POST']):
 # 	socketio.run(app, debug=True, port=5003)
 
 if __name__ == '__main__':
-	socketio.run(app, debug=True, port=5003, host='0.0.0.0')
+	app.run()
